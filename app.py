d0001f4500cf1c4141fcb09e909e24b0849b948a
Carla Daniela Nascimento <carla.danielaju@gmail.com>
2025-03-21 16:01:12 -0300

    Criando API e o banco de Dados

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+def init_db():
+    with sqlite3.connect('database.db') as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS livros (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT NOT NULL,
+                categoria TEXT NOT NULL,
+                autor TEXT NOT NULL,
+                imagem_url TEXT NOT NULL
+            )
+        ''')
+        
+init_db()
+
+@app.route('/')
+def home_page():
+    return "Bem-vindo à API de Livros!"
+
+@app.route('/doar', methods=['POST'])
+def doar_livros():
+    dados = request.json
+    
+    titulo = dados.get('titulo')
+    categoria = dados.get('categoria')
+    autor = dados.get('autor')
+    imagem_url = dados.get('imagem_url')
+
+    if not all([titulo, categoria, autor, imagem_url]): 
+        return jsonify({'erro': 'Todos os campos obrigatórios'}), 400 
+   
+    with sqlite3.connect('database.db') as conn:
+        conn.execute("""INSERT INTO livros (titulo, categoria, autor, imagem_url)
+                         VALUES (?, ?, ?, ?)""", (titulo, categoria, autor, imagem_url)) 
+        conn.commit()
+
+    return jsonify({'mensagem': 'livros cadastrados com sucesso'}), 201
+    
+@app.route('/livros', methods=['GET'])
+def listar_livros():
+    with sqlite3.connect('database.db') as conn:
+        livros = conn.execute("SELECT * FROM livros").fetchall()
+
+    livros_formatados = []
+    
+    for livro in livros:
+        dicionario_livros = {
+            "id": livro[0],
+            "titulo": livro[1],
+            "categoria": livro[2],
+            "autor": livro[3],
+            "imagem_url": livro[4]
+        }
+        livros_formatados.append(dicionario_livros)
+
+    return jsonify(livros_formatados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
